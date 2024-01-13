4f1e05a54b9a377c15a4684c6758341c095904f6
daeyeon <kdy4410121@naver.com>
2024-01-13 15:26:33 +0900

    commit

--- src/main/java/com/EatToday/EatToday/EatTodayApplication.java
+++ src/main/java/com/EatToday/EatToday/EatTodayApplication.java
@@ -1,14 +1,12 @@
 package com.EatToday.EatToday;
 
-<<<<<<< HEAD
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-=======
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
->>>>>>> 8a59aefde74c39e23c72adf7a4f5bb767f2b3378
+
 
 @SpringBootApplication
 public class EatTodayApplication {
