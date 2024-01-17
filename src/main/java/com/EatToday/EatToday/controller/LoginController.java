cc6a3a4d591d29566c8399a550a7e735e7035cb6
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-01-17 22:28:54 +0900

    회원가입 id, Nickname 중복 방지 기능 추가(수정)

--- src/main/java/com/EatToday/EatToday/controller/LoginController.java
+++ src/main/java/com/EatToday/EatToday/controller/LoginController.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -47,6 +48,24 @@ public class LoginController {
     public String Createuser(@Valid userForm form, BindingResult bindingResult) //파라미터의 위치는 @Valid 객체 바로 뒤에 선언해야 함 (중요)
     {
 
+        //중복 방지 기능
+        if(userService.checkuidDuplicate(form.getUid())) //id 중복
+        {
+            log.info("uid: "+ form.getUid()+" 중복");
+            bindingResult.addError(new FieldError("Signup","uid","ID 중복"));
+        }
+
+        if(userService.checkunameDuplicate(form.getUname())) //nickname 중복
+        {
+            log.info("uname: "+form.getUname()+" 중복");
+            bindingResult.addError(new FieldError("Signup","uname","Nickname 중복"));
+        }
+
+        if(bindingResult.hasErrors())
+        {
+            return "user/Signup";
+        }
+
         //위에 로깅 어노테이션 추가함으로써 아래 코드 필요없어짐, 아래 코드는 컨트롤러가 제대로 동작하는지 확인하기 위함
         log.info(toString());
        // System.out.println(form.toString());
@@ -59,20 +78,7 @@ public class LoginController {
         User saved = userRepository.save(user); //entity(user) 객체 반환받아서 saved 객체에 반환
         log.info(saved.toString());
 
-        //중복 방지 기능
-        if(userService.checkuidDuplicate(form.getUid()))
-        {
-            log.info("uid: "+ form.getUid()+" 중복");
-        }
 
-        if(userService.checkunameDuplicate(form.getUname()))
-        {
-                log.info("uname: "+form.getUname()+" 중복");
-        }
-
-        if(bindingResult.hasErrors()) { //만약 에러 위에서 에러 있으면
-            return "user/Signup";
-        }
 
         return "user/login";
     }
