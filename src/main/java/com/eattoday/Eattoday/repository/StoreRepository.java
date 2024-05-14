d678e8e78e8b243a911f0c580679fc56b32a0a97
daeyeon <kdy4410121@naver.com>
2024-05-14 15:52:38 +0900

    FEAT:create storelist 매장 조회 서비스와 repository 생성

--- src/main/java/com/eattoday/Eattoday/repository/StoreRepository.java
+++ src/main/java/com/eattoday/Eattoday/repository/StoreRepository.java
@@ -4,7 +4,9 @@ import com.eattoday.Eattoday.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface StoreRepository extends JpaRepository<Store,Long> {
-
+    ArrayList<Store> findAll();
 }
