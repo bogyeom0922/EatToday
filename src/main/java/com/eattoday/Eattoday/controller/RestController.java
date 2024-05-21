e91025e33488cab422e69e5a8e8c3725432b08a4
bogyeom0922 <pbk0398@gmail.com>
2024-05-21 17:10:49 +0900

    feat: 랜덤 매장 개수 3개로 변경

--- src/main/java/com/eattoday/Eattoday/controller/RestController.java
+++ src/main/java/com/eattoday/Eattoday/controller/RestController.java
@@ -29,10 +29,22 @@ public class RestController {
     public String category(Model model)
     {
         long count = storeRepository.count(); // store 개수 추출
-        int random = (int)((Math.random()*count));  // count까지의 숫자 중 랜덤 함수로 정수 추출
-        Long id=Long.valueOf(random); // id와 같은 Long타입으로 변경
-        Store store = storeRepository.findById(id).orElse(null);
-        model.addAttribute("randomStore", store); //model에 엔티티 값 저장
+
+        int random1 = (int)((Math.random()*count));  // count까지의 숫자 중 랜덤 함수로 정수 추출
+        Long id1=Long.valueOf(random1); // id와 같은 Long타입으로 변경
+        Store store1 = storeRepository.findById(id1).orElse(null);
+        model.addAttribute("randomStore1", store1); //model에 엔티티 값 저장
+
+        int random2 = (int)((Math.random()*count));  // count까지의 숫자 중 랜덤 함수로 정수 추출
+        Long id2=Long.valueOf(random2); // id와 같은 Long타입으로 변경
+        Store store2 = storeRepository.findById(id2).orElse(null);
+        model.addAttribute("randomStore2", store2); //model에 엔티티 값 저장
+
+        int random3 = (int)((Math.random()*count));  // count까지의 숫자 중 랜덤 함수로 정수 추출
+        Long id3=Long.valueOf(random3); // id와 같은 Long타입으로 변경
+        Store store3 = storeRepository.findById(id3).orElse(null);
+        model.addAttribute("randomStore3", store3); //model에 엔티티 값 저장
+
         return "rest/category";
     }
 
