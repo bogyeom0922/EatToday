9e58bc950f44140266754f7f7f7bfc25e816286d
daeyeon <kdy4410121@naver.com>
2024-05-23 13:23:16 +0900

    FEAT:create storelist 매장 조회 뷰 페이지에 페이지 번호 생성

--- src/main/java/com/eattoday/Eattoday/repository/StoreRepository.java
+++ src/main/java/com/eattoday/Eattoday/repository/StoreRepository.java
@@ -1,12 +1,18 @@
 package com.eattoday.Eattoday.repository;
 
 import com.eattoday.Eattoday.entity.Store;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store,Long> {
+    @Override
     ArrayList<Store> findAll();
+
 }
