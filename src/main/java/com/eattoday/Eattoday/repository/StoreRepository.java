e58fb9030c3d7c42ba21757c39470bd8d47bc6dc
daeyeon <kdy4410121@naver.com>
2024-05-23 13:31:59 +0900

    FEAT:create storelist-search바 findByAllContent 인터페이스 생성

--- src/main/java/com/eattoday/Eattoday/repository/StoreRepository.java
+++ src/main/java/com/eattoday/Eattoday/repository/StoreRepository.java
@@ -15,5 +15,6 @@ public interface StoreRepository extends JpaRepository<Store,Long> {
     @Override
     ArrayList<Store> findAll();
 
-
+    @Query(value = "select p from Store p where p.store_img Like %?1% OR p.store_name Like %?1% OR p.store_time Like %?1% OR p.store_phone Like %?1% OR p.store_star Like %?1%")
+    List<Store> findByAllContent(String keyword);
 }
