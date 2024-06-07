e3a1bf408651b0aa687d7c7a7674bd6999ba0678
bogyeom0922 <pbk0398@gmail.com>
2024-06-07 11:34:22 +0900

    FEAT: repository 수정 reviewrepository에 게시글을 매장 아이디, 유저 아이디별로 조회할 수 있도록 코드 추가

--- src/main/java/com/eattoday/Eattoday/repository/ReviewRepository.java
+++ src/main/java/com/eattoday/Eattoday/repository/ReviewRepository.java
@@ -2,7 +2,14 @@ package com.eattoday.Eattoday.repository;
 
 import com.eattoday.Eattoday.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Collection;
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-
+    //특정 게시글 모든 리뷰 조회
+    @Query(value = "SELECT * FROM Review WHERE store_id = :storeId", nativeQuery = true)
+    List<Review> findByStoreId(Long storeId);
+    List<Review> findByUserid(String userid);
 }
