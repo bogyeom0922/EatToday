8f33eb312c09a41b77c82007a3b4fe084e58d763
bogyeom0922 <pbk0398@gmail.com>
2024-06-07 11:23:01 +0900

    FEAT: controller에 user 데이터 등록

--- src/main/java/com/eattoday/Eattoday/controller/RestController.java
+++ src/main/java/com/eattoday/Eattoday/controller/RestController.java
@@ -1,8 +1,11 @@
 package com.eattoday.Eattoday.controller;
 
 import com.eattoday.Eattoday.entity.Store;
+import com.eattoday.Eattoday.entity.User;
 import com.eattoday.Eattoday.repository.StoreRepository;
+import com.eattoday.Eattoday.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,22 +15,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class RestController {
 
-    private final StoreRepository storeRepository;
+    @Autowired
+    private StoreRepository storeRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     public RestController(StoreRepository storeRepository) {
         this.storeRepository = storeRepository;
-    }
-
-    @GetMapping("/rest/{id}") //매장 상세페이지
-    public String show(@PathVariable("id") Long id, Model model) {
-
-        log.info("id = " + id); //매장 id 제대로 가져왔는지 log 확인
-
-        Store store = storeRepository.findById(id).orElse(null); // id 조회해 데이터 가져오기
-        model.addAttribute("rest", store); // 모델에 데이터 등록
-
-        return "rest/detail"; // 뷰 페이지 반환
-
     }
 
     @GetMapping("/category")
@@ -36,21 +31,38 @@ public class RestController {
         long count = storeRepository.count(); // store 개수 추출
 
         int random1 = (int) ((Math.random() * count));  // count까지의 숫자 중 랜덤 함수로 정수 추출
-        Long id1 = (long)random1; // id와 같은 Long타입으로 변경
+        Long id1 = (long) random1; // id와 같은 Long타입으로 변경
         Store store1 = storeRepository.findById(id1).orElse(null);
         model.addAttribute("randomStore1", store1); //model에 엔티티 값 저장
 
         int random2 = (int) ((Math.random() * count));  // count까지의 숫자 중 랜덤 함수로 정수 추출
-        Long id2 = (long)random2; // id와 같은 Long타입으로 변경
+        Long id2 = (long) random2; // id와 같은 Long타입으로 변경
         Store store2 = storeRepository.findById(id2).orElse(null);
         model.addAttribute("randomStore2", store2); //model에 엔티티 값 저장
 
         int random3 = (int) ((Math.random() * count));  // count까지의 숫자 중 랜덤 함수로 정수 추출
-        Long id3 = (long)random3; // id와 같은 Long타입으로 변경
+        Long id3 = (long) random3; // id와 같은 Long타입으로 변경
         Store store3 = storeRepository.findById(id3).orElse(null);
         model.addAttribute("randomStore3", store3); //model에 엔티티 값 저장
 
         return "rest/category";
+
+    }
+
+    @GetMapping("/rest/{id}/{uid}") //매장 상세페이지
+    public String show(@PathVariable("id") Long id, @PathVariable String uid, Model model) {
+
+        log.info("id = " + id); //매장 id 제대로 가져왔는지 log 확인
+        log.info("uid = "+uid);
+
+        Store store = storeRepository.findById(id).orElse(null); // id 조회해 데이터 가져오기
+        model.addAttribute("rest", store); // 모델에 데이터 등록
+
+        //user
+        User userEntity = userRepository.findByuid(uid).orElse(null);
+        model.addAttribute("user", userEntity);
+
+        return "rest/detail"; // 뷰 페이지 반환
 
     }
 
