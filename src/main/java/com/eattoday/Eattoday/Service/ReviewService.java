da32f0be519379c27dfb6d7f2ce6841b20557e83
bogyeom0922 <pbk0398@gmail.com>
2024-06-07 11:35:36 +0900

    FEAT: ReviewService 수정
    리뷰 수정 기능 추가

--- src/main/java/com/eattoday/Eattoday/Service/ReviewService.java
+++ src/main/java/com/eattoday/Eattoday/Service/ReviewService.java
@@ -39,4 +39,18 @@ public class ReviewService {
         //4. DTO로 변환해 반환
         return ReviewDto.createReviewDto(created);
     }
+
+    @Transactional
+    public ReviewDto update(Long id, ReviewDto dto) {
+        //1. 리뷰 조회
+        Review target = reviewRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("댓글 수정 실패!" +
+                        "대상 댓글이 없습니다."));
+        //2. 리뷰 수정
+        target.patch(dto);
+        //3. DB로 갱신
+        Review updated = reviewRepository.save(target);
+        //4. 리뷰 엔티티를 DTO로 변환 및 반환
+        return ReviewDto.createReviewDto(updated);
+    }
 }
