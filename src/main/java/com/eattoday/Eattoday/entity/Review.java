73bbfe5cded2217f99ea230eff56bf54f9b640d9
bogyeom0922 <pbk0398@gmail.com>
2024-06-07 11:37:21 +0900

    FEAT: Review 수정
    엔티티에 리뷰 생성 기능 추가
    예외 발생시 메시지 전송

--- src/main/java/com/eattoday/Eattoday/entity/Review.java
+++ src/main/java/com/eattoday/Eattoday/entity/Review.java
@@ -1,5 +1,6 @@
 package com.eattoday.Eattoday.entity;
 
+import com.eattoday.Eattoday.dto.ReviewDto;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -22,5 +23,20 @@ public class Review {
     private String userid;
     @Column
     private String body;
+
+    public static Review createReview(ReviewDto dto, Store store) {
+        //예외 발생
+        if(dto.getId()!=null) {
+            throw new IllegalArgumentException("리뷰 생성 실패! 리뷰의 id가 없어야 합니다.");
+        }
+        //엔티티 생성 및 반환
+        return new Review(
+                dto.getId(),
+                store,
+                dto.getStoreName(),
+                dto.getUserid(),
+                dto.getBody()
+        );
+    }
 
 }
