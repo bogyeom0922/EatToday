9949a6c476dea31094b5fa8b596cf1c453c19d99
bogyeom0922 <pbk0398@gmail.com>
2024-06-28 18:26:41 +0900

    FEAT: 리뷰별 매장 정보 조회

--- src/main/java/com/eattoday/Eattoday/controller/LoginController.java
+++ src/main/java/com/eattoday/Eattoday/controller/LoginController.java
@@ -116,6 +116,11 @@ public class LoginController {
         List<Review> reviews = reviewRepository.findByUserid(uid); //uid로 리뷰 목록 조회
         List<Store> review_store = new ArrayList<>(); // 각 리뷰에 해당되는 매장 정보를 저장할 리스트 생성
 
+        for (Review review : reviews) { // 각 리뷰에 대해 매장 정보 조회 후 저장
+            review_store.add(review.getStore());
+        }
+
+
         return "userinfo/info_review";
 
     }
