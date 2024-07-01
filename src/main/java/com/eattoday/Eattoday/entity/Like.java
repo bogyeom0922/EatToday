6769a1554721b814808bbe0516bfe0a13aa126cd
daeyeon <kdy4410121@naver.com>
2024-07-01 13:43:48 +0900

    FEAT:fix createLike 함수에 사용되는 파라미터는 클래스에 선언한 순서대로 해야함

--- src/main/java/com/eattoday/Eattoday/entity/Like.java
+++ src/main/java/com/eattoday/Eattoday/entity/Like.java
@@ -10,21 +10,21 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "likes")
+@Entity(name = "Likes")
 
 public class Like {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
-    @JoinColumn(name = "store_id")
-    private Store store;
-    @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
     private long state;
 
-    public static Like createLike(LikeDto likeDto, User user, Store store, long state){
+    public static Like createLike(LikeDto likeDto, User user, Store store, long state) {
         return new Like(likeDto.getId(), user, store, state);
     }
 }
