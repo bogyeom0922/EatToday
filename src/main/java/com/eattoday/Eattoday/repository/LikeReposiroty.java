8924da71796b6afa6678bfe8f8284b4269d5cdf3
daeyeon <kdy4410121@naver.com>
2024-07-01 13:51:21 +0900

    FEAT:create LikeRepository 생성

--- src/main/java/com/eattoday/Eattoday/repository/LikeReposiroty.java
+++ src/main/java/com/eattoday/Eattoday/repository/LikeReposiroty.java
@@ -1,4 +1,10 @@
 package com.eattoday.Eattoday.repository;
 
-public interface LikeReposiroty {
+import com.eattoday.Eattoday.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LikeReposiroty extends JpaRepository<Like,Long> {
+
 }
