5384e96e8b61d00f67a183713cac60ef2c6f41a3
daeyeon <kdy4410121@naver.com>
2024-07-01 13:24:10 +0900

    FEAT:create like 엔티티에 store 변수 생성

--- src/main/java/com/eattoday/Eattoday/entity/Like.java
+++ src/main/java/com/eattoday/Eattoday/entity/Like.java
@@ -1,9 +1,6 @@
 package com.eattoday.Eattoday.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,5 +15,8 @@ public class Like {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
 
 }
