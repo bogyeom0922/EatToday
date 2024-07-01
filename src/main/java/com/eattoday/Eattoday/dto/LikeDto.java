b77a6c70bb8bd96738208fc78444bf4544b2d6a3
daeyeon <kdy4410121@naver.com>
2024-07-01 13:08:55 +0900

    FEAT:create 기본 lombok 추가

--- src/main/java/com/eattoday/Eattoday/dto/LikeDto.java
+++ src/main/java/com/eattoday/Eattoday/dto/LikeDto.java
@@ -1,5 +1,17 @@
 package com.eattoday.Eattoday.dto;
 
+import jakarta.persistence.GeneratedValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
 public class LikeDto {
+
 
 }
