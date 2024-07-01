2e68b17a9dac7d29524bab20587229cf4c58a5b7
daeyeon <kdy4410121@naver.com>
2024-07-01 13:45:30 +0900

    FEAT:create like 엔티티를 dto로 변환해주는 함수 생성

--- src/main/java/com/eattoday/Eattoday/dto/LikeDto.java
+++ src/main/java/com/eattoday/Eattoday/dto/LikeDto.java
@@ -1,5 +1,6 @@
 package com.eattoday.Eattoday.dto;
 
+import com.eattoday.Eattoday.entity.Like;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.GeneratedValue;
 import lombok.AllArgsConstructor;
@@ -14,13 +15,20 @@ import lombok.Setter;
 
 public class LikeDto {
     private long id;
-
     @JsonProperty("storeId")
     private Long store_id;
     @JsonProperty("userId")
     private Long user_id;
     private Long state;
 
+    public static LikeDto createLikeDto(Like like){
+        return new LikeDto(
+                like.getId(),
+                like.getStore().getId(),
+                like.getUser().getId(),
+                like.getState()
+        );
+    }
 
 
 }
