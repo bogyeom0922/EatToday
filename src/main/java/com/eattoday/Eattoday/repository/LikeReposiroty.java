6588eb6fe85eabd6e5b7020c54370227bf57e011
daeyeon <kdy4410121@naver.com>
2024-07-03 11:26:28 +0900

    FEAT:create 매장 이름과 유저 이름을 이용해 좋아요를 찾는 인터페이스 생성

--- src/main/java/com/eattoday/Eattoday/repository/LikeReposiroty.java
+++ src/main/java/com/eattoday/Eattoday/repository/LikeReposiroty.java
@@ -2,9 +2,11 @@ package com.eattoday.Eattoday.repository;
 
 import com.eattoday.Eattoday.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LikeReposiroty extends JpaRepository<Like,Long> {
-
+    @Query(value = "select * from Likes where user_id = ?1 and store_id = ?2", nativeQuery = true)
+    Like findByUser_idAndStore_id(Long user_id, Long store_id);
 }
