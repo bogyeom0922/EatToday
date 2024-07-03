c810b9609af57cf0f87063e48efcae438aa5f332
daeyeon <kdy4410121@naver.com>
2024-07-03 11:32:55 +0900

    FEAT:create user의 id로 like를 한 목록을 찾는 인터페이스 생성

--- src/main/java/com/eattoday/Eattoday/repository/LikeReposiroty.java
+++ src/main/java/com/eattoday/Eattoday/repository/LikeReposiroty.java
@@ -5,8 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface LikeReposiroty extends JpaRepository<Like,Long> {
     @Query(value = "select * from Likes where user_id = ?1 and store_id = ?2", nativeQuery = true)
     Like findByUser_idAndStore_id(Long user_id, Long store_id);
+
+    @Query(value = "select * from Likes l where user_id = ?1", nativeQuery = true)
+    ArrayList<Like> findLikes(Long user_id);
 }
