0d8061e7cfbedb994ecbe001b3f3980296689ca9
daeyeon <kdy4410121@naver.com>
2024-07-25 15:33:23 +0900

    FEAT:create 어노테이션 추가

--- src/main/java/com/eattoday/Eattoday/api/LikeApiController.java
+++ src/main/java/com/eattoday/Eattoday/api/LikeApiController.java
@@ -1,4 +1,11 @@
 package com.eattoday.Eattoday.api;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j //로그를 찍기 위한 어노테이션
+@RestController
+
 public class LikeApiController {
+
 }
