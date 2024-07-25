d307ae55cfd184bd084eaf6b640c676fbc671ebf
daeyeon <kdy4410121@naver.com>
2024-07-25 15:57:07 +0900

    FEAT:create 발견한 like가 null이 아닐 경우에만 삭제해줌(혹시 모를 오류 대비)

--- src/main/java/com/eattoday/Eattoday/service/LikeService.java
+++ src/main/java/com/eattoday/Eattoday/service/LikeService.java
@@ -61,7 +61,8 @@ public class LikeService {
         User user = userRepository.findById(user_id)
                 .orElseThrow(() ->new IllegalArgumentException("유저 정보 없음"));
         Like like = likeReposiroty.findByUser_idAndStore_id(user.getId(), store.getId());
-        likeReposiroty.delete(like);
+        if(like != null)
+            likeReposiroty.delete(like);
         return LikeDto.createLikeDto(like);
     }
 
