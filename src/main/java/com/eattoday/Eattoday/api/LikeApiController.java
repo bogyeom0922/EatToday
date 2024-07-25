14a4134587bceeac0c86835fed2ce1a217f3702a
daeyeon <kdy4410121@naver.com>
2024-07-25 15:36:06 +0900

    FEAT:create 좋아요가 눌려있는지 확인하는 Likes 함수 생성

--- src/main/java/com/eattoday/Eattoday/api/LikeApiController.java
+++ src/main/java/com/eattoday/Eattoday/api/LikeApiController.java
@@ -1,11 +1,27 @@
 package com.eattoday.Eattoday.api;
 
+import com.eattoday.Eattoday.dto.LikeDto;
+import com.eattoday.Eattoday.repository.StoreRepository;
+import com.eattoday.Eattoday.service.LikeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j //로그를 찍기 위한 어노테이션
 @RestController
 
 public class LikeApiController {
+    @Autowired
+    private LikeService likeService;
+    @Autowired
+    private StoreRepository storeRepository;
+
+    @GetMapping("api/{id}/{storeId}/likes")
+    public ResponseEntity<LikeDto> Likes(@PathVariable Long id, @PathVariable Long storeId){
+
+    }
 
 }
