47b6a21fa4a80506060250e6b6f3d8920c5b5115
bogyeom0922 <pbk0398@gmail.com>
2024-07-29 09:43:51 +0900

    FEAT: 비밀번호 확인

--- src/main/java/com/eattoday/Eattoday/service/UserService.java
+++ src/main/java/com/eattoday/Eattoday/service/UserService.java
@@ -33,6 +33,26 @@ public class UserService {
         // 1. 회원이 입력한 uid로 db조회
         Optional<User> userOptional = userRepository.findByuid(form.getUid());
 
+        // 2. db에 있는 pw와 입력한 pw 같은지 확인
+        if (userOptional.isPresent()) //조회 결과가 있는 경우
+        {
+            User user = userOptional.get(); // optional 벗겨 내고 entity 객체 가져옴
+
+            // 2. db에 있는 pw와 입력한 pw 같은지 확인
+            if (user.getUpassword().equals(form.getUpassword())) {
+                //entity, dto 일치할 경우
+                //entity -> dto 변환 후 리턴
+                UserForm dto = UserForm.toUserFrom(user);
+
+                return dto;
+            } else { //비밀번호 불일치
+                return null;
+            }
+
+        } else { //조회 결과가 없는 경우
+            return null;
+        }
+
     }
 
 }
