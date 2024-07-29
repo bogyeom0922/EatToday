5fbc8e5cb36e58abacaf8408ce19d66fddacbccf
bogyeom0922 <pbk0398@gmail.com>
2024-07-29 09:50:58 +0900

    FEAT: return 경로 수정
    close: #54

--- src/main/java/com/eattoday/Eattoday/controller/LoginController.java
+++ src/main/java/com/eattoday/Eattoday/controller/LoginController.java
@@ -77,7 +77,7 @@ public class LoginController {
 
             log.info("Login successful for user: {}", loginResult.getUname());
 
-            return "redirect:/category/" + loginResult.getId();
+            return "redirect:/category/" + loginResult.getUid();
         } //로그인 실패
         else {
             log.info("Login failed");
@@ -127,7 +127,7 @@ public class LoginController {
             return "user/login";
         }
 
-        //위에 로깅 어노테이션 추가함으로써 아래 코드 필요없어짐, 아래 코드는 컨트롤러가 제대로 동작하는지 확인하기 위함
+        //컨트롤러가 제대로 동작하는지 확인하기 위함
         log.info(toString());
         // System.out.println(form.toString());
 
@@ -154,7 +154,7 @@ public class LoginController {
 
     }
 
-    @GetMapping("/{uid}/review") //마이페이지_리뷰
+    @GetMapping("/{uid}/review") //마페이지_리뷰
     public String info_review(@PathVariable String uid, Model model) {
 
         User userEntity = userRepository.findByuid(uid).orElse(null);
