e2f600b69c4c0b730ecf52f719e7b51eadb7c1c2
bogyeom0922 <pbk0398@gmail.com>
2024-07-29 09:41:45 +0900

    FEAT: uid 조회

--- src/main/java/com/eattoday/Eattoday/service/UserService.java
+++ src/main/java/com/eattoday/Eattoday/service/UserService.java
@@ -1,9 +1,12 @@
 package com.eattoday.Eattoday.service;
 
 import com.eattoday.Eattoday.dto.UserForm;
+import com.eattoday.Eattoday.entity.User;
 import com.eattoday.Eattoday.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor //생성자 어노테이션 userRepository
@@ -26,6 +29,9 @@ public class UserService {
 
     //로그인 서비스
     public UserForm login(UserForm form) {
+
+        // 1. 회원이 입력한 uid로 db조회
+        Optional<User> userOptional = userRepository.findByuid(form.getUid());
 
     }
 
