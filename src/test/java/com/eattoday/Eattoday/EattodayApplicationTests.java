2849e3578553d7aedaef9007fe4416feddaed031
daeyeon <kdy4410121@naver.com>
2025-01-17 17:00:11 +0900

    feat: 깃 변경사항 적용

--- src/test/java/com/eattoday/Eattoday/EattodayApplicationTests.java
+++ src/test/java/com/eattoday/Eattoday/EattodayApplicationTests.java
@@ -1,13 +1,1 @@
-package com.eattoday.Eattoday;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EattodayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
