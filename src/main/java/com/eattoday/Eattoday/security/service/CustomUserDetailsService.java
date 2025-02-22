10e2890148947b1d7d05a110132498b39d649aaa
daeyeon <kdy4410121@naver.com>
2025-02-22 20:51:01 +0900

    feat: authentication 정보 return 해주는 함수

--- src/main/java/com/eattoday/Eattoday/security/service/CustomUserDetailsService.java
+++ src/main/java/com/eattoday/Eattoday/security/service/CustomUserDetailsService.java
@@ -3,6 +3,8 @@ package com.eattoday.Eattoday.security.service;
 import com.eattoday.Eattoday.security.dto.CustomUserDetails;
 import com.eattoday.Eattoday.user.domain.User;
 import com.eattoday.Eattoday.user.repository.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -23,8 +25,21 @@ public class CustomUserDetailsService implements UserDetailsService {
 
         User userData = userRepository.findByUserId(username);
 
-        if (userData != null) {
-            return new CustomUserDetails(userData);
+        if (userData == null) {
+            System.out.println("user null");
+            throw new UsernameNotFoundException("user 없음");
+        }
+
+        return new CustomUserDetails(userData);
+    }
+
+    public User getCurrentUserFromSecurityContext() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null) {
+            CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+            System.out.println("authentication user" + customUserDetails.getUser());
+            return customUserDetails.getUser();
         }
 
         return null;
