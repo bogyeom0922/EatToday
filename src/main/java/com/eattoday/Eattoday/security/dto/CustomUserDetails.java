40b1c5e258fbfc07bf892fa0a5711afbd4cca041
daeyeon <kdy4410121@naver.com>
2025-02-22 20:50:23 +0900

    fix: 시큐리티 사용을 위해 getAuthorities 함수 수정

--- src/main/java/com/eattoday/Eattoday/security/dto/CustomUserDetails.java
+++ src/main/java/com/eattoday/Eattoday/security/dto/CustomUserDetails.java
@@ -1,12 +1,16 @@
 package com.eattoday.Eattoday.security.dto;
 
 import com.eattoday.Eattoday.user.domain.User;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
+@Getter
 public class CustomUserDetails implements UserDetails {
 
     private final User user;
@@ -17,18 +21,9 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        Collection<GrantedAuthority> collection = new ArrayList<>();
-
-        // 역할을 설정할 때 기본값을 ROLE_USER로 설정
-        collection.add(new GrantedAuthority() {
-            @Override
-            public String getAuthority() {
-                return "ROLE_USER"; // 기본 역할 설정
-            }
-        });
-
-        return collection;
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));  // 권한 추가
     }
+
 
     @Override
     public String getPassword() {
