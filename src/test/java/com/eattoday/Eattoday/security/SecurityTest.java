70a29b56ffdbc5100f590a8ad1205a18397933b4
daeyeon <kdy4410121@naver.com>
2025-03-06 16:32:53 +0900

    feat: 토큰을 통해 시큐리티 인증 통과 테스트 코드

--- src/test/java/com/eattoday/Eattoday/security/SecurityTest.java
+++ src/test/java/com/eattoday/Eattoday/security/SecurityTest.java
@@ -7,18 +7,22 @@ import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.http.Cookie;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -37,6 +41,8 @@ public class SecurityTest {
     private JWTUtil JwtUtil;
 
     private static final String WRONG_KEY = "kdjqkwdjklasnfklasnflknqwipdwdasfsfasfqwffqw";
+    private static final String KEY = "asdsafaskfsjkgqwgqwdpqwojdpwjdasldkasdklnqwddwqdasdasdasdwqfgd"
+    ;
 
     @Test
     public void 로그인_성공_테스트() throws Exception{
@@ -97,6 +103,25 @@ public class SecurityTest {
                 .withMessageContaining("JWT signature does not match");
     }
 
+    @Test
+    @DisplayName("토큰 기간 expired 되었는지 검사")
+    public void checkTokenExpired(){
+        String token = SecurityTest.createExpiredToken();
+
+        assertThat(JwtUtil.isExpired(token)).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("로그인 후 얻은 쿠키를 통해 다른 api 시큐리티 인증 테스트")
+    public void checkSecurityFromCookie() throws Exception{
+        Cookie authorizationCookie = getLoginCookie();
+        assertThat(authorizationCookie).isNotNull();
+
+        mockMvc.perform(get("/category")
+                .cookie(authorizationCookie))
+                .andExpect(status().isOk());
+    }
+
     public static String createInvalidSignatureJwt() {
         return Jwts.builder()
                 .setSubject("user1")
@@ -104,5 +129,26 @@ public class SecurityTest {
                 .setExpiration(new Date(System.currentTimeMillis() + 60000))
                 .signWith(SignatureAlgorithm.HS256, WRONG_KEY)
                 .compact();
+    }
+
+    public static String createExpiredToken(){
+        return Jwts.builder()
+                .setSubject("11")
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() - 60000))
+                .signWith(SignatureAlgorithm.HS256, KEY)
+                .compact();
+    }
+
+    private Cookie getLoginCookie() throws Exception{
+        LoginRequest loginRequest = new LoginRequest("11", "11");
+
+        MvcResult result = mockMvc.perform(post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginRequest)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        return result.getResponse().getCookie("Authorization");
     }
 }
